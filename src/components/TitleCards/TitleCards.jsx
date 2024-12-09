04ce5e4d400cdb0d2104ac31336c933d1ae60bc6
KoradaVyshnavi <koradavyshnavi3@gmail.com>
2024-12-09 16:14:47 +0530

    First Commit

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef, useState } from 'react'
+import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
+import { Link } from 'react-router-dom';
+
+const TitleCards = ({title, category}) => {
+
+  const [apiData, setApiData] = useState([]);
+  const cardsRef = useRef();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MWQ4MTEwYWEzNWY2ZjA0MmNmNDZkZTNmN2MxMjg0MyIsIm5iZiI6MTczMzQyMjIxNi4yOTc5OTk5LCJzdWIiOiI2NzUxZWM4ODgwMmJhZDE2MDkxYTkzMjgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.UlCg8oyj-pb9_dSS6b_bi97aUgr15S8YGbntf2RijWc'
+    }
+  };
+
+  const handleWheel = (event)=>{
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  }
+
+  useEffect(()=>{
+
+    fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results))
+    .catch(err => console.error(err));
+
+    cardsRef.current.addEventListener('wheel', handleWheel);
+  },[])
+
+  return (
+    <div className='title-cards'>
+      <h2>{title?title:"Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {apiData.map((card, index)=>{
+          return <Link to={`/player/${card.id}`} className="card" key={index}>
+            <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+            <p>{card.original_title}</p>
+          </Link>
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default TitleCards
---
+++ src/firebase.js
@@ -0,0 +1,55 @@
+import { initializeApp } from "firebase/app";
+import { 
+    createUserWithEmailAndPassword, 
+    getAuth, 
+    signInWithEmailAndPassword, 
+    signOut} from "firebase/auth";
+import {
+    addDoc, 
+    collection, 
+    getFirestore } from "firebase/firestore";
+import { toast } from "react-toastify";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAIq0-gAULtiwbD9NjOHuEY6SfKvSuXswc",
+  authDomain: "netflix-clone-92a4b.firebaseapp.com",
+  projectId: "netflix-clone-92a4b",
+  storageBucket: "netflix-clone-92a4b.firebasestorage.app",
+  messagingSenderId: "457391248929",
+  appId: "1:457391248929:web:29699b07247252bf4ab560"
+};
+
+const app = initializeApp(firebaseConfig);  
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (name, email, password)=>{
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const user = res.user;
+        await addDoc(collection(db, "user"), {
+            uid: user.uid,
+            name,
+            authProvider: "local",
+            email,
+        });
+    } catch (error) {
+        console.log(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+    }
+}
+
+const login = async(email, password)=>{
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+        console.log(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+    }
+}
+
+const logout = ()=>{
+    signOut(auth);
+}
+
+export {auth, db, login, signup, logout};
